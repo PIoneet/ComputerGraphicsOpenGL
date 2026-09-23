808e3c99a8a38d9f5b589c7ca09df53adcf9722a
Sun Hong Jae <sunhongjae@gmail.com>
2026-09-23 21:24:49 +0900

    # 2026 09 23 file5.cpp 구현 완료했습니다.

--- ComputerGraphicsOpenGL5/file5.cpp
+++ ComputerGraphicsOpenGL5/file5.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <glm/glm.hpp>
 #include <random>
+#include <algorithm>
 
 std::random_device rd;
 std::mt19937 gen(rd());
@@ -12,9 +13,14 @@ std::uniform_real_distribution<float> disColor{ 0.0f, 1.0f };
 
 std::uniform_real_distribution<float> disR{ 0.08f, 0.18f };
 
+std::uniform_int_distribution<int> disNum{20, 40}; // 랜덤으로 생성될 사각형의 개수
 
-#define WIDTH  1400.0
-#define HEIGHT 1400.0
+std::uniform_real_distribution<float> disPos{ -1.0f, 1.0f };
+
+
+#define WIDTH  1600.0
+#define HEIGHT 1600.0
+#define RECT_SIZE 0.04
 
 
 struct Rect {
@@ -39,11 +45,13 @@ struct Rect {
 - 2.8 r을 누르면 20~40개 사각형도 다시 생성하고 다 삭제하고 지우개 사각형도 초기화 해야 한다. 
 */
 
+void isOverlap();
+void generateRect();
 void TransformScreenToNDC(double& i, double& j);
 void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
-//void CursorPosCallback(GLFWwindow* window, double xpos, double ypos);
+void CursorPosCallback(GLFWwindow* window, double xpos, double ypos);
 void DrawScene();
 
 
@@ -53,6 +61,7 @@ Rect eraseRect; // 지우기 사각형
 
 bool Dragged{false};
 glm::vec2 mousePos{};
+int genRect{};
 
 
 int main()
@@ -88,10 +97,15 @@ int main()
 
     glfwSetKeyCallback(window, KeyCallback);
     glfwSetMouseButtonCallback(window, MouseButtonCallback);
+    glfwSetCursorPosCallback(window, CursorPosCallback);
 
 
-    randRects.reserve(50);
+    randRects.reserve(100);
+    generateRect();
 
+    eraseRect.r = 2 * RECT_SIZE;
+    eraseRect.rgb = glm::vec3{ 0.0f, 0.0f, 0.0f };
+    eraseRect.isDraw = false;
 
 
     while (!glfwWindowShouldClose(window)) {
@@ -108,6 +122,53 @@ int main()
 
 
 }
+
+
+
+
+void isOverlap()
+{
+    for (int i = 0; i < randRects.size(); ++i)
+    {
+        if (eraseRect.x + eraseRect.r <= randRects[i].x - randRects[i].r ||
+            randRects[i].x + randRects[i].r <= eraseRect.x - eraseRect.r ||
+            eraseRect.y + eraseRect.r <= randRects[i].y - randRects[i].r ||
+            randRects[i].y + randRects[i].r <= eraseRect.y - eraseRect.r ||
+            randRects[i].isDraw == false)
+        {
+            std::cout << "사각형이 겹치지 않았습니다." << std::endl;
+            
+        }
+        else {
+            std::cout << "사각형이 겹쳤습니다." << " ";
+            std::cout << "사각형 인덱스: " << i << " " << std::endl;
+
+            randRects[i].isDraw = false;
+            eraseRect.rgb = randRects[i].rgb;
+            eraseRect.r += RECT_SIZE / 4;
+            
+        }
+
+    }
+
+}
+
+
+
+void generateRect()
+{
+    int randNum = disNum(gen);
+
+    for (int i = 0; i < randNum; ++i)
+    {
+        glm::vec3 randColor{disColor(gen), disColor(gen), disColor(gen)};
+
+        randRects.push_back(Rect{ disPos(gen), disPos(gen), RECT_SIZE, randColor });
+    }
+}
+
+
+
 
 
 void TransformScreenToNDC(double& i, double& j)
@@ -131,6 +192,17 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
     case GLFW_KEY_Q:
         glfwSetWindowShouldClose(window, GLFW_TRUE);
         break;
+    case GLFW_KEY_R:
+    // 다 삭제하고 초기화하기       
+        randRects.clear();
+        generateRect();
+        eraseRect.r = 2 * RECT_SIZE;
+        eraseRect.rgb = glm::vec3{0.0f, 0.0f, 0.0f};
+        eraseRect.isDraw = false;
+        genRect = 0;
+
+        break;
+    
     }
 
 }
@@ -142,12 +214,65 @@ void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
     {
         double xpos, ypos;
         glfwGetCursorPos(window, &xpos, &ypos);
-    }
+    
+    
+        TransformScreenToNDC(xpos, ypos);
 
+        eraseRect.x = xpos;
+        eraseRect.y = ypos;
+        eraseRect.isDraw = true;
+        eraseRect.rgb = glm::vec3{ 0.0f,0.0f,0.0f };
+
+
+        Dragged = true;
+
+        mousePos.x = xpos;
+        mousePos.y = ypos;
+        
+    
+    }
+    else if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+    {
+        if (genRect < 10)
+        {
+            double xpos, ypos;
+            glfwGetCursorPos(window, &xpos, &ypos);
+
+            TransformScreenToNDC(xpos, ypos);
+
+            glm::vec3 randColor{ disColor(gen), disColor(gen), disColor(gen) };
+
+            randRects.push_back(Rect{ (float)xpos, (float)ypos, RECT_SIZE, randColor });
+
+
+            eraseRect.r = std::max( eraseRect.r - RECT_SIZE / 2, RECT_SIZE/2 );
+
+            ++genRect;
+        }
+
+
+    }
+    else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+    {
+        Dragged = false;
+
+
+        for (auto& rect : randRects)
+        {
+            rect.isDraw = true;
+
+        }
+
+        eraseRect.isDraw = false;
+    }
+    
+  
+    
 }
 
 
-/*
+
+
 void CursorPosCallback(GLFWwindow* window, double xpos, double ypos)
 {
     if (Dragged)
@@ -157,30 +282,41 @@ void CursorPosCallback(GLFWwindow* window, double xpos, double ypos)
         std::pair<double, double> move{ xpos - mousePos.x , ypos - mousePos.y };
         // 현재 위치 xpos에서 이전 위치 mousePos.x를 빼서 이동량을 구해야 한다. 
 
-        randRects[rectNum].x1 = randRects[rectNum].x1 + move.first;
-        randRects[rectNum].y1 = randRects[rectNum].y1 + move.second;
+        isOverlap();
+
+        eraseRect.x = eraseRect.x + move.first;
+        eraseRect.y = eraseRect.y + move.second;
 
         mousePos.x = xpos;
         mousePos.y = ypos;
 
     }
 }
-*/
+
+
+
 
 void DrawScene()
 {
-    glClearColor(0.6f, 0.6f, 0.6f, 1.0f);
+    glClearColor(0.4f, 0.4f, 0.4f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     for (auto& rect : randRects)
     {
+        if (rect.isDraw == false)
+            continue;
+
         glColor3f(rect.rgb.x, rect.rgb.y, rect.rgb.z);
         glRectf(rect.x - rect.r, rect.y - rect.r, rect.x + rect.r, rect.y + rect.r);
 
     }
 
-    glColor3f(eraseRect.rgb.x, eraseRect.rgb.y, eraseRect.rgb.z);
-    glRectf(eraseRect.x - eraseRect.r, eraseRect.y - eraseRect.r, eraseRect.x + eraseRect.r, eraseRect.y + eraseRect.r);
+    if (eraseRect.isDraw != false)
+    {
+        glColor3f(eraseRect.rgb.x, eraseRect.rgb.y, eraseRect.rgb.z);
+        glRectf(eraseRect.x - eraseRect.r, eraseRect.y - eraseRect.r, eraseRect.x + eraseRect.r, eraseRect.y + eraseRect.r);
+    }
+    
 
 
 }
